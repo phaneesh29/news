aed7749f28bd81d24fe533e150105d41e75d7d9b
Sreephaneesh <kanugovisreephaneesha@gmail.com>
2026-06-11 22:43:48 +0530

    new whilteliste

--- newsFetch/whitelistEmails.js
+++ newsFetch/whitelistEmails.js
@@ -1,5 +1,6 @@
 export const whitelistedEmails = [
   'sreephaneesha2005@gmail.com',
   'vivekgowdashivakumar@gmail.com',
-  'chandan005ae@gmail.com'
+  'chandan005ae@gmail.com',
+  'ssaisreeharis@gmail.com',
 ];
