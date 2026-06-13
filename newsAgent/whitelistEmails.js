88f471d6a0b2263667f3f677fe9b7ee37f4f27a0
phaneesh29 <kanugovisreephaneesha@gmail.com>
2026-06-13 23:36:29 +0530

    chore: update whitelistEmails.js to include all recipients

--- newsAgent/whitelistEmails.js
+++ newsAgent/whitelistEmails.js
@@ -1,7 +1,7 @@
 export const whitelistedEmails = [
   'sreephaneesha2005@gmail.com',
-  // 'vivekgowdashivakumar@gmail.com',
-  // 'chandan005ae@gmail.com',
-  // 'ssaisreeharis@gmail.com',
-  // 'pradumnahj8@gmail.com'
+  'vivekgowdashivakumar@gmail.com',
+  'chandan005ae@gmail.com',
+  'ssaisreeharis@gmail.com',
+  'pradumnahj8@gmail.com'
 ];
