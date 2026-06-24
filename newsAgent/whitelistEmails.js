f5a80dce45984de507f5715d66311f1a8b7434c9
phaneesh29 <kanugovisreephaneesha@gmail.com>
2026-06-24 21:20:11 +0530

    remove all whitelisted users

--- newsAgent/whitelistEmails.js
+++ newsAgent/whitelistEmails.js
@@ -1,8 +1,3 @@
 export const whitelistedEmails = [
-  'sreephaneesha2005@gmail.com',
-  'vivekgowdashivakumar@gmail.com',
-  'chandan005ae@gmail.com',
-  'ssaisreeharis@gmail.com',
-  'pradumnahj8@gmail.com',
-  'sreepad1975@gmail.com'
+  'sreephaneesha2005@gmail.com'
 ];
