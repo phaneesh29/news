efa90636d599dec9b49f634d6481911483ccb3db
phaneesh29 <kanugovisreephaneesha@gmail.com>
2026-06-25 19:56:28 +0530

    chore: Remove test-giget folder

--- userApp/client/web/test-giget/clients/resend.client.js
+++
@@ -1,6 +0,0 @@
-import { Resend } from 'resend';
-import { RESEND_API_KEY } from '../config/index.js';
-
-const resend = new Resend(RESEND_API_KEY);
-
-export { resend };
--- userApp/client/web/test-giget/controllers/health.controller.js
+++
@@ -1,9 +0,0 @@
-import { NODE_ENV } from '../config/index.js';
-
-export function getHealth(_req, res) {
-	res.status(200).json({
-		ok: true,
-		environment: NODE_ENV,
-		timestamp: new Date().toISOString(),
-	});
-}
